8a5ccb6ba73884acdb7615263a35b0e55bf484b4
Vivek Ananth RP <vivekananthrp@gmail.com>
2017-09-05 00:07:25 +0530

    Completed assignment

--- code.py
+++ code.py
@@ -5,10 +5,11 @@ def fourth_element_of_list(L):
     """
     Return the nth element of ``L`` if it exists, ``None`` otherwise.
     """
-    if len(L) < i:
-        return None
-    else:
+    try:
         return L[i]
+    except IndexError:
+        print ('Requested index {} is beyond the length of the list.'.format(i))
+        return None
 
 # 2
 # Modify to use try-except to return the sum of all numbers in L,
@@ -18,12 +19,15 @@ def sum_of_numbers(L):
     """
     Return the sum of all the numbers in L.
     """
+    
     s = 0
     for l in L:
-        if type(l) is int or type(l) is float:
+        try:
             s += l
+        except TypeError:
+            print ('Ignoring - {}, as it is neither an integer nor float.'.format(l))
     return s
 
 # TEST
-# sum_of_numbers([3, 1.9, 's']) == 4.9
+print (sum_of_numbers([3, 1.9, 's']) == 4.9)
 
